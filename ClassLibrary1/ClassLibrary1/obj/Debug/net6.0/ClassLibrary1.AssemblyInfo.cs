00e3b0df265a1eb01102dabb4ea7b25ea7baa9b8
MinhNH57 <nutmsotn04@gmail.com>
2024-10-03 15:00:08 +0700

    1

--- ClassLibrary1/ClassLibrary1/obj/Debug/net6.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/ClassLibrary1/obj/Debug/net6.0/ClassLibrary1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b991122b0105c64f2e996712e5f9b9650294bd49")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
